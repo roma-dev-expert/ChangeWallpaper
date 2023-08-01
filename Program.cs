4d70797a1f85ceb0b277411b5944f81e0c580d8b
roma-dev-expert <roma.k.devexpert@gmail.com>
2023-08-01 17:43:44 +0300

    Translate comments in Program file

--- Program.cs
+++ Program.cs
@@ -1,38 +1,39 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 public class Program
 {
-    // Константы для SystemParametersInfo
+    // Constants for SystemParametersInfo
     private const int SPI_SETDESKWALLPAPER = 0x0014;
     private const int SPIF_UPDATEINIFILE = 0x01;
     private const int SPIF_SENDCHANGE = 0x02;
 
-    // Импортируем функцию SystemParametersInfo из библиотеки user32.dll
+    // Import the SystemParametersInfo function from user32.dll
     [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
     private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
 
     public static void Main()
     {
-        // Путь к новой заставке рабочего стола
+        // Path to the new desktop wallpaper
         string wallpaperPath = @"C:\Users\rkhma\Pictures\Wallpapers\bmw_e30_m3_97062_1920x1080.jpg";
 
-        // Вызываем функцию для установки новой заставки рабочего стола
+        // Call the function to set the new desktop wallpaper
         bool result = SetWallpaper(wallpaperPath);
 
         if (result)
         {
-            Console.WriteLine("Заставка рабочего стола изменена успешно.");
+            Console.WriteLine("Desktop wallpaper changed successfully.");
         }
         else
         {
-            Console.WriteLine("Не удалось изменить заставку рабочего стола.");
+            Console.WriteLine("Failed to change desktop wallpaper.");
         }
     }
 
-    // Функция для изменения заставки рабочего стола
+    // Function to change the desktop wallpaper
     public static bool SetWallpaper(string wallpaperPath)
     {
-        // Вызываем SystemParametersInfo для установки новой заставки
+        // Call SystemParametersInfo to set the new wallpaper
         int result = SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, wallpaperPath, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
 
         return result != 0;
